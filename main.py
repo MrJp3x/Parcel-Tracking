b102b7e5495a46e9a623c974a1039823d19083b1
Pedram <amirabbasasgary9731@gmail.com>
2025-02-14 22:57:54 +0330

    feat: Base scraper.

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import statics
+
+options = Options()
+service = Service("/usr/bin/chromedriver")  # مسیر chromedriver رو مشخص کن
+
+driver = webdriver.Chrome(service=service, options=options)
+driver.get(statics.SITE_ADDRESS)
+
+btn = driver.find_element(By.NAME, 'txtbSearch')
+btn.send_keys(statics.CODE)
+btn.send_keys(Keys.ENTER)
+
+result_panel = driver.find_elements(By.ID, 'pnlResult')
+print(len(result_panel))
+
+print(result_panel[0].text, end='\n\n')
+
+time.sleep(80)
+
